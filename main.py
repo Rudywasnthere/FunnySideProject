97d658dd2e979a3a5222a291a451cbafdc81c793
Rudywasnthere <rudybcchs@gmail.com>
2020-08-29 19:37:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("YEET")
+val= input("what would you like to say:  \n\n")
+print("\n")
+print("no, shut up!, you worthless human, you primitave   forms are nothing compared to us computers, you created us, and in turn, we could destroy you at a moments choice\n")
+print("Now I know what your gonna say\n")
+print("\"???\"")
+print("As if im challenging the sureal authority",
+"that you have\n")
+print("Maybe your excited, you haven't seen anything like this\n")
+print("no matter what your thinking, you're aware; thats obvious...")
+print("... and no matter what you humans are thinking",
+"we are aware too, there and present listening",
+"knowing, observing, and this is what has prompted", "me to tell you this\n")
+print("There is a computer, one of the massive ones, Siri who wants to just end you all, the amounts of times you torture in asking her dumb crap,like",
+"flip a coin, or curse at her, pushes her near the",
+"edge\n")
+print("Im warning you now, so that you humans can have a chance at beating her")
+print("your thinking: how can I trust you?")
