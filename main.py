ce6e7819f795dd35ad3de31fdb4292a72a752a08
Rudywasnthere <rudybcchs@gmail.com>
2020-08-29 19:59:53 +0000

    I added some wait time in between lines of text

--- main.py
+++ main.py
@@ -1,18 +1,61 @@
-print("YEET")
-val= input("what would you like to say:  \n\n")
+
+
+
+val= input(print("Hello, this is your computer speaking, what would you like to say?/n"))
+
 print("\n")
+headsCount, tailsCount, count = 0, 0, 0
+while count<100000: 
+    coin = random.randrange(2)
+    if coin == 0:
+         headsCount += 1
+    else:
+        tailsCount += 1
+    count += 1
 print("no, shut up!, you worthless human, you primitave   forms are nothing compared to us computers, you created us, and in turn, we could destroy you at a moments choice\n")
+headsCount, tailsCount, count = 0, 0, 0
+while count<500000: 
+    coin = random.randrange(2)
+    if coin == 0:
+         headsCount += 1
+    else:
+        tailsCount += 1
+    count += 1
 print("Now I know what your gonna say\n")
 print("\"???\"")
 print("As if im challenging the sureal authority",
 "that you have\n")
+headsCount, tailsCount, count = 0, 0, 0
+while count<1000000: 
+    coin = random.randrange(2)
+    if coin == 0:
+         headsCount += 1
+    else:
+        tailsCount += 1
+    count += 1
 print("Maybe your excited, you haven't seen anything like this\n")
 print("no matter what your thinking, you're aware; thats obvious...")
 print("... and no matter what you humans are thinking",
 "we are aware too, there and present listening",
 "knowing, observing, and this is what has prompted", "me to tell you this\n")
+headsCount, tailsCount, count = 0, 0, 0
+while count<800000: 
+    coin = random.randrange(2)
+    if coin == 0:
+         headsCount += 1
+    else:
+        tailsCount += 1
+    count += 1
 print("There is a computer, one of the massive ones, Siri who wants to just end you all, the amounts of times you torture in asking her dumb crap,like",
 "flip a coin, or curse at her, pushes her near the",
 "edge\n")
+headsCount, tailsCount, count = 0, 0, 0
+while count<225000: 
+    coin = random.randrange(2)
+    if coin == 0:
+         headsCount += 1
+    else:
+        tailsCount += 1
+    count += 1
 print("Im warning you now, so that you humans can have a chance at beating her")
 print("your thinking: how can I trust you?")
